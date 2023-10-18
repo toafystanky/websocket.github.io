f6c8a06cf1850827809fbfca0788dcd3d7ca34ea
Franziska Wolf <wolf@spsejecna.cz>
2023-10-18 21:58:09 +0200

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ print("Server listening on Port " + str(PORT))
 
 connected = set()
 banned_clients = set()  # Set to store banned client IPs
-client_ips = {}  # Dictionary to track client IPs
 message_history = []
 
 async def notify_clients(message):
@@ -24,16 +23,8 @@ async def echo(websocket, path):
         await websocket.close()
         return
 
-    # Check if the client IP is already connected
-    if client_ip in client_ips and client_ips[client_ip] != websocket:
-        print(f"Client IP {client_ip} is already connected. Sending alert message.")
-        await websocket.send("You are already connected elsewhere.")
-        return
-
     connected.add(websocket)
     client_id = len(connected)  # Assign the client ID based on the number of connected clients
-
-    client_ips[client_ip] = websocket
 
     try:
         await notify_clients(f"Client {client_id} has just connected")
@@ -48,10 +39,9 @@ async def echo(websocket, path):
             if "rum" in message.lower():
                 banned_clients.add(client_ip)
                 await websocket.send("You are permanently banned.")
+                await websocket.close()
                 if websocket in connected:
                     connected.remove(websocket)
-                if client_ip in client_ips:
-                    del client_ips[client_ip]
 
                 return
 
@@ -61,8 +51,6 @@ async def echo(websocket, path):
     finally:
         if websocket in connected:
             connected.remove(websocket)
-        if client_ip in client_ips and client_ips[client_ip] == websocket:
-            del client_ips[client_ip]  # Remove client IP from the dictionary when the client disconnects
 
 start_server = websockets.serve(echo, "0.0.0.0", PORT)
 asyncio.get_event_loop().run_until_complete(start_server)
